41d43a02a341d96356760f185f604fc6fdc3f5bb
Rupankar Das <88283289+rupnkrdas@users.noreply.github.com>
2023-05-26 14:33:38 +0530

    Time: 9 ms (30.99%), Space: 45.1 MB (14.87%) - LeetHub

--- 877-stone-game/877-stone-game.java
+++ 877-stone-game/877-stone-game.java
@@ -1,32 +1,31 @@
 class Solution {
-    int[][] dp;
     public boolean stoneGame(int[] piles) {
         int n = piles.length;
-        dp = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                dp[i][j] = -1;
-            }
-        }
-        return f(0, n - 1, piles) > 0 ? true : false;
+        return f(piles, n) > 0 ? true : false;
     } 
     
-    public int f(int i, int j, int[] p) {
-        if (i == j) {
-            return p[i];
+    public int f(int[] p, int n) {
+        int[][] dp = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = n - 1; j >= i; j--) {
+                if (i == j) {
+                    dp[i][j] = p[i];
+                    continue;
+                }
+                
+                int ans = Integer.MIN_VALUE;
+        
+                // take from first
+                int op1 = p[i] - dp[i + 1][j];
+                // take from last
+                int op2 = p[j] - dp[i][j - 1];
+
+                ans = Math.max(ans, Math.max(op1, op2));
+
+                dp[i][j] = ans;
+            }
         }
         
-        if (dp[i][j] != -1) return dp[i][j];
-        
-        int ans = Integer.MIN_VALUE;
-        
-        // take from first
-        int op1 = p[i] - f(i + 1, j, p);
-        // take from last
-        int op2 = p[j] - f(i, j - 1, p);
-        
-        ans = Math.max(ans, Math.max(op1, op2));
-        
-        return dp[i][j] = ans;
+        return dp[0][n - 1];
     }
 }
