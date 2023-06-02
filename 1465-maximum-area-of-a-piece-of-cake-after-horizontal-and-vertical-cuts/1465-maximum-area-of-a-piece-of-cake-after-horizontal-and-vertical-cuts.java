49a1d057b20476eb25d1368daa91cf4aee5d9773
Rupankar Das <88283289+rupnkrdas@users.noreply.github.com>
2023-06-02 13:05:24 +0530

    Time: 17 ms (58.79%), Space: 54.2 MB (24.85%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.java
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.java
@@ -1,92 +1,35 @@
 class Solution {
-    long mod = (long) (1e9 + 7);
+    long mod = (int)1e9 + 7;
     public int maxArea(int h, int w, int[] horizontalCuts, int[] verticalCuts) {
         Arrays.sort(horizontalCuts);
+        long[] hor = new long[horizontalCuts.length + 2];
+        int n = hor.length;
+        hor[0] = 0;
+        hor[n - 1] = h;
+        for (int i = 0; i < horizontalCuts.length; i++) {
+            hor[i + 1] = horizontalCuts[i];
+        }
+
         Arrays.sort(verticalCuts);
-        long[] nsl1 = findNSL(horizontalCuts);
-        long[] ngr1 = findNGR(horizontalCuts, h);
-        long[] nsl2 = findNSL(verticalCuts);
-        long[] ngr2 = findNGR(verticalCuts, w);
-
-        // System.out.println(Arrays.toString(nsl1));
-        // System.out.println(Arrays.toString(ngr1));
-        // System.out.println(Arrays.toString(nsl2));
-        // System.out.println(Arrays.toString(ngr2));
-
-        long maxHeight = Integer.MIN_VALUE;
-        for (int i = 0; i < nsl1.length; i++) {
-            maxHeight = Math.max(maxHeight, horizontalCuts[i] - nsl1[i]) % mod;
-        }
-        for (int i = 0; i < ngr1.length; i++) {
-            maxHeight = Math.max(maxHeight, ngr1[i] - horizontalCuts[i]) % mod;
+        long[] ver = new long[verticalCuts.length + 2];
+        int m = ver.length;
+        ver[0] = 0;
+        ver[m - 1] = w;
+        for (int i = 0; i < verticalCuts.length; i++) {
+            ver[i + 1] = verticalCuts[i];
         }
 
-        long maxWidth = Integer.MIN_VALUE;
-        for (int i = 0; i < nsl2.length; i++) {
-            maxWidth = Math.max(maxWidth, verticalCuts[i] - nsl2[i]) % mod;
-        }
-        for (int i = 0; i < ngr2.length; i++) {
-            maxWidth = Math.max(maxWidth, ngr2[i] - verticalCuts[i]) % mod;
+        long maxHeight = Long.MIN_VALUE;
+        for (int i = 0; i <= hor.length - 2; i++) {
+            maxHeight = Math.max(maxHeight, hor[i + 1] - hor[i]);
         }
 
-        return (int) (maxHeight % mod * maxWidth % mod);
-    }
-
-    public long[] findNGR(int[] arr, long right) {
-        int n = arr.length;
-        long[] ans = new long[n];
-        Stack<Integer> st = new Stack<>();
-
-        for (int i = n - 1; i >= 0; i--) {
-            if (st.isEmpty()) {
-                ans[i] = right;
-            } else {
-                if (st.peek() > arr[i]) {
-                    ans[i] = st.peek();
-                } else {
-                    while (!st.isEmpty() && st.peek() <= arr[i]) {
-                        st.pop();
-                    }
-                    if (st.isEmpty()) {
-                        ans[i] = right;
-                    } else {
-                        ans[i] = st.peek();
-                    }
-                }
-            }
-
-            st.push(arr[i]);
+        long maxWidth = Long.MIN_VALUE;
+        for (int i = 0; i <= ver.length - 2; i++) {
+            maxWidth = Math.max(maxWidth, ver[i + 1] - ver[i]);
         }
 
-        return ans;
-    }
-    
-    public long[] findNSL(int[] arr) {
-        int n = arr.length;
-        long[] ans = new long[n];
-        Stack<Integer> st = new Stack<>();
+        return (int)((maxHeight % mod) * (maxWidth % mod) % mod);
 
-        for (int i = 0; i < n; i++) {
-            if (st.isEmpty()) {
-                ans[i] = 0;
-            } else {
-                if (st.peek() < arr[i]) {
-                    ans[i] = st.peek();
-                } else {
-                    while (!st.isEmpty() && st.peek() >= arr[i]) {
-                        st.pop();
-                    }
-                    if (st.isEmpty()) {
-                        ans[i] = 0;
-                    } else {
-                        ans[i] = st.peek();
-                    }
-                }
-            }
-
-            st.push(arr[i]);
-        }
-
-        return ans;
     }
 }
