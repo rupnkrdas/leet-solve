578c652776abca9b5d7655e881da5755069bc77a
Rupankar Das <88283289+rupnkrdas@users.noreply.github.com>
2023-06-18 21:08:40 +0530

    Time: 67 ms (75.00%), Space: 44.4 MB (75.00%) - LeetHub

--- 2742-painting-the-walls/2742-painting-the-walls.java
+++ 2742-painting-the-walls/2742-painting-the-walls.java
@@ -1,7 +1,14 @@
 class Solution {
-    Map<String, Integer> dp = new HashMap<>();
+    int[][] dp;
+    // Map<String, Integer> dp = new HashMap<>();
     public int paintWalls(int[] cost, int[] time) {
         int n = cost.length;
+        dp = new int[n + 1][n + 1];
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= n; j++) {
+                dp[i][j] = -1;
+            }
+        }
         return f(0, cost, time, n);
     }
 
@@ -13,16 +20,18 @@ class Solution {
             return (int)1e9;
         }
 
-        String key = Integer.toString(i) + "," + Integer.toString(wallsRemaining);
-        if (dp.containsKey(key)) {
-            return dp.get(key);
-        }
+        // String key = Integer.toString(i) + "," + Integer.toString(wallsRemaining);
+        // if (dp.containsKey(key)) {
+        //     return dp.get(key);
+        // }
+        if (dp[i][wallsRemaining] != -1) return dp[i][wallsRemaining];
 
         int take = cost[i] + f(i + 1, cost, time, wallsRemaining - time[i] - 1);
         int notTake = f(i + 1, cost, time, wallsRemaining);
 
-        int ans = Math.min(take, notTake);
-        dp.put(key, ans);
-        return ans;
+        // int ans = Math.min(take, notTake);
+        // dp.put(key, ans);
+        // return ans;
+        return dp[i][wallsRemaining] = Math.min(take, notTake);
     }
 }
