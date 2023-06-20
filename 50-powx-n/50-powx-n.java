9fd43a9e178a61dfb24a340eb43316604e68ef65
Rupankar Das <88283289+rupnkrdas@users.noreply.github.com>
2023-06-20 12:22:45 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (91.63%) - LeetHub

---
+++ 50-powx-n/50-powx-n.java
@@ -0,0 +1,15 @@
+class Solution {
+    public double myPow(double x, int n) {
+        double res = f(x, Math.abs(n));
+        return (n >= 0) ? res : (double)1/res;
+    }
+
+    public double f(double x, int n) {
+        if (x == 0) return 0;
+        if (n == 0) return 1;
+
+        double res = f(x*x, n/2);
+        res *= (n % 2 != 0) ? x : 1;
+        return res;
+    }
+}
