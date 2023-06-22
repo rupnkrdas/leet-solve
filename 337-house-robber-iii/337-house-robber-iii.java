7f26e1c4088a237f0eba65aa623fbeeed1fd54d0
Rupankar Das <88283289+rupnkrdas@users.noreply.github.com>
2023-06-22 13:25:05 +0530

    Time: 0 ms (100.00%), Space: 43.9 MB (42.22%) - LeetHub

--- 337-house-robber-iii/337-house-robber-iii.java
+++ 337-house-robber-iii/337-house-robber-iii.java
@@ -14,30 +14,25 @@
  * }
  */
 class Solution {
-    Map<TreeNode, Map<Integer, Integer>> map = new HashMap<>();
-    public int rob(TreeNode root ) {
-        return Math.max(f(root, 0), f(root, 1));
+    public int rob(TreeNode root) {
+        int ans[] = solve(root);
+        return Math.max(ans[0], ans[1]);
     }
 
-    public int f(TreeNode node, int canTake) {
-        if (node == null) return 0;
-
-        if (map.containsKey(node) && map.get(node).containsKey(canTake)) {
-            return map.get(node).get(canTake);
+    // solve() returns two values in an array, arr[0] = max if take this node, arr[1] = max if not take this node
+    // this is our hypothesis
+    public int[] solve(TreeNode node) {
+        if (node == null) {
+            return new int[]{0, 0};
         }
 
-        int val = 0;
-        if (canTake == 1) {
-            val = Math.max(val, node.val + f(node.left, 0) + f(node.right, 0));
-        } 
-        val = Math.max(val, f(node.left, 1) + f(node.right, 1));
+        int[] left = solve(node.left);
+        int[] right = solve(node.right);
+        int[] ans = new int[2];
 
-        if (!map.containsKey(node)) {
-            map.put(node, new HashMap<>());
-            map.get(node).put(canTake, val);
-        } else {
-            map.get(node).put(canTake, val);
-        }
-        return val;
+        ans[0] = node.val + left[1] + right[1];
+        ans[1] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+
+        return ans;
     }
 }
