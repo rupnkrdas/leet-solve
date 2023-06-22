6c6ba3d4acfea2ac2fee81518707d36376380df4
Rupankar Das <88283289+rupnkrdas@users.noreply.github.com>
2023-06-22 10:37:28 +0530

    Time: 5 ms (44.87%), Space: 41.6 MB (41.50%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -1,49 +1,6 @@
 class Solution {
-    Map<Integer, List<Character>> map = new HashMap<>();
+    String[] map = {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
     public List<String> letterCombinations(String digits) {
-        List<Character> list = new ArrayList<>();
-        list.add('a');
-        list.add('b');
-        list.add('c');
-        map.put(2, new ArrayList<>(list));
-        list.clear();
-        list.add('d');
-        list.add('e');
-        list.add('f');
-        map.put(3, new ArrayList<>(list));
-        list.clear();
-        list.add('g');
-        list.add('h');
-        list.add('i');
-        map.put(4, new ArrayList<>(list));
-        list.clear();
-        list.add('j');
-        list.add('k');
-        list.add('l');
-        map.put(5, new ArrayList<>(list));
-        list.clear();
-        list.add('m');
-        list.add('n');
-        list.add('o');
-        map.put(6, new ArrayList<>(list));
-        list.clear();
-        list.add('p');
-        list.add('q');
-        list.add('r');
-        list.add('s');
-        map.put(7, new ArrayList<>(list));
-        list.clear();
-        list.add('t');
-        list.add('u');
-        list.add('v');
-        map.put(8, new ArrayList<>(list));
-        list.clear();
-        list.add('w');
-        list.add('x');
-        list.add('y');
-        list.add('z');
-        map.put(9, new ArrayList<>(list));
-        list.clear();
 
         List<String> ans = new ArrayList<>();
         f(0, "", digits, ans);
@@ -58,7 +15,8 @@ class Solution {
             return;
         }
 
-        for (char c : map.get(digits.charAt(idx) - '0')) {
+        for (int i = 0; i < map[digits.charAt(idx)-'0'].length(); i++) {
+            char c = map[digits.charAt(idx)-'0'].charAt(i);
             s += c;
             f(idx + 1, s, digits, ans);
             s = s.substring(0, s.length() - 1);
