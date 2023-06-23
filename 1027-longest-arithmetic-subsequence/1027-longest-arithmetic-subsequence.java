115c5024deaf28eee772fa89907df97ff3cd4732
Rupankar Das <88283289+rupnkrdas@users.noreply.github.com>
2023-06-23 23:08:35 +0530

    Time: 81 ms (84.03%), Space: 55.1 MB (74.53%) - LeetHub

--- 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.java
+++ 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.java
@@ -1,36 +1,17 @@
 class Solution {
-    public int longestArithSeqLength(int[] arr) {
-        int n = arr.length;
-        if (n <= 2)
-            return n;
+    public int longestArithSeqLength(int[] nums) {
+        int[][] dp = new int[1001][1002]; // dp[index][difference]
+        int maxi = 2;
 
-        Map<Integer, Integer> map = new HashMap<>(); // (nums[i], i)
-        int[][] dp = new int[1001][1001];
-        int ans = 1;
-
-        // Iterate over the array
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                int diff = arr[j] - arr[i];
-                int need = arr[i] - diff;
-
-                // Check if there is a previous element (need) in the map that forms an arithmetic subsequence with the current pair (i, j).
-                if (map.containsKey(need)) {
-                    // If a previous element exists, update the length of the arithmetic subsequence ending at (i, j).
-                    dp[i][j] = Math.max(dp[map.get(need)][i] + 1, dp[i][j]);
-                } else {
-                    // If a previous element doesn't exist, initialize the length of the arithmetic subsequence ending at (i, j) to 2.
-                    dp[i][j] = 2;
-                }
-
-                // Update the maximum length of the arithmetic subsequence encountered so far.
-                ans = Math.max(ans, dp[i][j]);
+        int n = nums.length;
+        for (int index = 1; index < n; index++) {
+            for (int prevIndex = 0; prevIndex < index; prevIndex++) {
+                int diff = nums[index] - nums[prevIndex] + 501;
+                dp[index][diff] = (dp[prevIndex][diff] > 0) ? (1 + dp[prevIndex][diff]) : 2;
+                maxi = Math.max(maxi, dp[index][diff]);
             }
-            
-            // Store the current element and its index in the map.
-            map.put(arr[i], i);
         }
 
-        return ans;
+        return maxi;
     }
 }
