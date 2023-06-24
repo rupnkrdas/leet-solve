aba913129dc1be4b9b58803296e3157b501788de
Rupankar Das <88283289+rupnkrdas@users.noreply.github.com>
2023-06-24 18:45:53 +0530

    Time: 933 ms (6.56%), Space: 166.3 MB (6.56%) - LeetHub

--- 956-tallest-billboard/956-tallest-billboard.java
+++ 956-tallest-billboard/956-tallest-billboard.java
@@ -1,8 +1,14 @@
 class Solution {
-    Map<String, Integer> map = new HashMap<>();
+    // Map<String, Integer> map = new HashMap<>();
+    int[][] dp = new int[1001][10003];
     public int tallestBillboard(int[] rods) {
-        int ans = f(0, rods, 0);
+        for (int i = 0; i < 1001; i++) {
+            for (int j = 0; j < 10003; j++) {
+                dp[i][j] = -1;
+            }
+        }
 
+        int ans = f(0, rods, 0);
         return ans/2;
     }
 
@@ -11,17 +17,19 @@ class Solution {
             if (diff == 0) return 0;
             else return (int)(-1e9);
         }
+        
+        if (dp[idx][diff + 5000] != -1) return dp[idx][diff + 5000];
 
-        String key = Integer.toString(idx) + "," + Integer.toString(diff);
-        if (map.containsKey(key)) return map.get(key);
+        // String key = Integer.toString(idx) + "," + Integer.toString(diff);
+        // if (map.containsKey(key)) return map.get(key);
 
 
         int take1 = nums[idx] + f(idx + 1, nums, diff + nums[idx]);
         int take2 = nums[idx] + f(idx + 1, nums, diff - nums[idx]);
         int notTake = f(idx + 1, nums, diff);
 
-        int ans = Math.max(take1, Math.max(take2, notTake));
-        map.put(key, ans);
-        return ans;
+        return dp[idx][diff + 5000] = Math.max(take1, Math.max(take2, notTake));
+        // map.put(key, ans);
+        // return ans;
     }
 }
