bdaafe837c64b6492fde79ab15c7166b360115b7
Rupankar Das <88283289+rupnkrdas@users.noreply.github.com>
2023-06-25 20:24:50 +0530

    Time: 58 ms (92.00%), Space: 43.9 MB (12.00%) - LeetHub

---
+++ 1575-count-all-possible-routes/1575-count-all-possible-routes.java
@@ -0,0 +1,31 @@
+class Solution {
+    int MOD = (int)(1e9+7);
+    int endCity;
+    int[][] dp = new int[101][401];
+    public int countRoutes(int[] locations, int start, int finish, int fuel) {
+        endCity = finish;
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+        int ans = f(start, fuel, locations, locations.length);
+        return ans % MOD;
+    }
+
+    public int f(int city, int fuel, int[] loc, int n) {
+        if (fuel < 0) return 0;
+
+        if (dp[city][fuel + 200] != -1) return dp[city][fuel + 200];
+
+        // choices
+        // 0 <- n-1, i != city
+        long ways = 0;
+        if (city == endCity) ways += 1;
+        for (int i = 0; i < n; i++) {
+            if (i != city) {
+                ways += f(i, fuel - Math.abs(loc[i]-loc[city]), loc, n);
+            }
+        }
+
+        return dp[city][fuel + 200] = (int)(ways % MOD);
+    }
+}
