e2bc2adda7b2d8be8a0cec36bdca9e37f292f426
Rupankar Das <88283289+rupnkrdas@users.noreply.github.com>
2023-06-26 20:08:23 +0530

    Time: 5 ms (12.68%), Space: 45.7 MB (7.84%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.java
+++ 547-number-of-provinces/547-number-of-provinces.java
@@ -1,40 +1,63 @@
+class DS {
+    int[] parent;
+    int[] size;
+
+    public DS(int n) {
+        parent = new int[n + 1];
+        size = new int[n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    public int find(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = find(parent[node]);
+    }
+
+    public void union(int u, int v) {
+        int uu = find(u);
+        int uv = find(v);
+
+        if (size[uu] < size[uv]) {
+            parent[uu] = uv;
+            size[uv] += size[uu];
+        } else {
+            parent[uv] = uu;
+            size[uu] += size[uv];
+        }
+    }
+}
 class Solution {
-    public int findCircleNum(int[][] isConnected) {
-        int n = isConnected.length;
+    public int findCircleNum(int[][] edges) {
         List<List<Integer>> adj = new ArrayList<>();
-        
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < edges.length; i++) {
             adj.add(new ArrayList<>());
         }
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i != j && isConnected[i][j] == 1) {
+        for (int i = 0; i < edges.length; i++) {
+            for (int j = 0; j < edges.length; j++) {
+                if (i != j && edges[i][j] == 1) {
                     adj.get(i).add(j);
-                    adj.get(j).add(i);
                 }
             }
         }
 
-        int[] vis = new int[n];
-        int count = 0;
-        for (int i = 0; i < n; i++) {
-            if (vis[i] == 0) {
-                count++;
-                dfs(i, vis, adj);
+        DS ds = new DS(adj.size());
+        for (int i = 0; i < adj.size(); i++) {
+            for (int v : adj.get(i)) {
+                ds.union(i, v);
             }
+        }
+
+        int count = 0;
+        for (int i = 0; i < adj.size(); i++) {
+            if (ds.find(i) == i)
+                count++;
         }
 
         return count;
-    }
-
-    public void dfs(int node, int[] vis, List<List<Integer>> adj) {
-        vis[node] = 1;
-
-        for (int adjNode : adj.get(node)) {
-            if (vis[adjNode] == 0) {
-                dfs(adjNode, vis, adj);
-            }
-        }
     }
 }
