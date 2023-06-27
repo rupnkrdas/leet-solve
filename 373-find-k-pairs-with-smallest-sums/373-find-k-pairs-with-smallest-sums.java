a07fea28a06c96ac9fd337d9883deed6cc73d3f6
Rupankar Das <88283289+rupnkrdas@users.noreply.github.com>
2023-06-27 16:35:16 +0530

    Time: 32 ms (96.80%), Space: 61.1 MB (12.46%) - LeetHub

--- 373-find-k-pairs-with-smallest-sums/373-find-k-pairs-with-smallest-sums.java
+++ 373-find-k-pairs-with-smallest-sums/373-find-k-pairs-with-smallest-sums.java
@@ -11,32 +11,34 @@ class Tuple implements Comparable<Tuple> {
 
     @Override
     public int compareTo(Tuple other) {
-        return (this.first + this.second) - (other.first + other.second);
+        return this.first - other.first;
     }
 }
 class Solution {
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-        PriorityQueue<Tuple> minHeap = new PriorityQueue<>();
+        int n = nums1.length;
+        int m = nums2.length;
+        PriorityQueue<Tuple> heap = new PriorityQueue<>();
+        heap.offer(new Tuple(nums1[0] + nums2[0], 0, 0));
         List<List<Integer>> ans = new ArrayList<>();
-        if (nums1.length == 0 || nums2.length == 0 || k == 0) return ans;
-        for (int i = 0; i < nums1.length && i < k; i++) {
-            minHeap.offer(new Tuple(nums1[i], nums2[0], 0));
-        }
-        while (k-- > 0 && !minHeap.isEmpty()) {
-            Tuple tuple = minHeap.poll();
-            int x = tuple.first;
-            int y = tuple.second;
-            int index = tuple.third;
-            List<Integer> list = new ArrayList<>();
-            list.add(x);
-            list.add(y);
-            ans.add(list);
 
-            if (index == nums2.length - 1) {
-                continue;
+        while (k-- > 0 && !heap.isEmpty()) {
+            Tuple t = heap.poll();
+            int i = t.second, j = t.third;
+            List<Integer> pair = new ArrayList<>();
+            pair.add(nums1[i]);
+            pair.add(nums2[j]);
+            ans.add(pair);
+
+            if (i == j) { // go right, go left, go diagonal --> think of i,j to be forming a grid hypothetically
+                if (i + 1 < n) heap.offer(new Tuple(nums1[i + 1] + nums2[j], i + 1, j));
+                if (j + 1 < m) heap.offer(new Tuple(nums1[i] + nums2[j + 1], i, j + 1));
+                if (i + 1 < n && j + 1 < m) heap.offer(new Tuple(nums1[i + 1] + nums2[j + 1], i + 1, j + 1));
+            } else if (i > j && i + 1 < n) {
+                heap.offer(new Tuple(nums1[i + 1] + nums2[j], i + 1, j));
+            } else if (j > i && j + 1 < m) {
+                heap.offer(new Tuple(nums1[i] + nums2[j + 1], i, j + 1));
             }
-
-            minHeap.offer(new Tuple(x, nums2[index + 1], index + 1));
         }
 
         return ans;
