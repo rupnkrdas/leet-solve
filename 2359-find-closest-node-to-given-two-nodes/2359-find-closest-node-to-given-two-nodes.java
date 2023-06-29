dc1330d31e221dbb13a2e68ff80a1345d68c50ba
Rupankar Das <88283289+rupnkrdas@users.noreply.github.com>
2023-06-29 12:36:41 +0530

    Time: 148 ms (14.09%), Space: 85.7 MB (7.91%) - LeetHub

--- 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.java
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.java
@@ -7,11 +7,12 @@ class Pair {
         this.second = second;
     }
 }
+
 class Solution {
-    public void dfs(int node, List<List<Integer>> adj, int distance, int[] dist, int[] vis) {
+    public void dfs(int node, ArrayList<Integer>[] adj, int distance, int[] dist, int[] vis) {
         vis[node] = 1;
 
-        for (int adjNode : adj.get(node)) {
+        for (int adjNode : adj[node]) {
             if (vis[adjNode] == 0) {
                 if (distance + 1 < dist[adjNode]) {
                     dist[adjNode] = distance + 1;
@@ -23,34 +24,30 @@ class Solution {
 
     public int closestMeetingNode(int[] edges, int node1, int node2) {
         int n = edges.length;
-        List<List<Integer>> adj = new ArrayList<>();
+        ArrayList<Integer>[] adj = new ArrayList[n];
         for (int i = 0; i < n; i++) {
-            adj.add(new ArrayList<>());
+            adj[i] = new ArrayList<>();
         }
         for (int i = 0; i < n; i++) {
             int u = i;
             int v = edges[i];
 
             if (v != -1) {
-                adj.get(u).add(v);
+                adj[u].add(v);
             }
         }
-
-        // System.out.println(adj);
 
         int[] dist1 = new int[n];
         int[] vis = new int[n];
         Arrays.fill(dist1, Integer.MAX_VALUE);
         dist1[node1] = 0;
         dfs(node1, adj, 0, dist1, vis);
-        System.out.println(Arrays.toString(dist1));
 
         int[] dist2 = new int[n];
         Arrays.fill(dist2, Integer.MAX_VALUE);
         dist2[node2] = 0;
         Arrays.fill(vis, 0);
         dfs(node2, adj, 0, dist2, vis);
-        System.out.println(Arrays.toString(dist2));
 
         int ans = -1;
         int minDistanceTillNode = Integer.MAX_VALUE;
