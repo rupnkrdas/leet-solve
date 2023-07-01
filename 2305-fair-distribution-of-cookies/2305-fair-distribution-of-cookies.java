ca10638b81eb8e0b0c463c53cede322364f72ec2
Rupankar Das <88283289+rupnkrdas@users.noreply.github.com>
2023-07-01 16:37:01 +0530

    Time: 435 ms (21.88%), Space: 39.9 MB (69.27%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.java
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.java
@@ -1,27 +1,26 @@
 class Solution {
     int[] children;
-    public int solve(int index, int[] cookies, int k) {
+    int res = (int)1e9;
+    public void solve(int index, int[] cookies, int k) {
         if (index == cookies.length) {
             int maxi = children[0];
             for (int num : children) {
                 maxi = Math.max(maxi, num);
             }
 
-            return maxi;
+            res = Math.min(res, maxi);
+            return;
         }
 
-        int mini = (int)(1e9);
         for (int i = 0; i < k; i++) {
             children[i] += cookies[index];
-            int op = solve(index + 1, cookies, k);
+            solve(index + 1, cookies, k);
             children[i] -= cookies[index];
-            mini = Math.min(mini, op);
         }
-
-        return mini;
     }
     public int distributeCookies(int[] cookies, int k) {
         children = new int[k];
-        return solve(0, cookies, k);
+        solve(0, cookies, k);
+        return res;
     }
 }
