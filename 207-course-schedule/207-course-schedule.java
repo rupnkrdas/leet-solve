7c1e96cf6333a9ddc30cbcf3b48361ff7a644c3d
Rupankar Das <88283289+rupnkrdas@users.noreply.github.com>
2023-07-13 09:01:58 +0530

    Time: 4 ms (79.65%), Space: 44.8 MB (13.06%) - LeetHub

--- 207-course-schedule/207-course-schedule.java
+++ 207-course-schedule/207-course-schedule.java
@@ -1,42 +1,26 @@
-
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        int V = numCourses;
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-
-        for (int i = 0; i < V; i++) {
+        int n = numCourses;
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
             adj.add(new ArrayList<>());
         }
-
-        for (int i = 0; i < prerequisites.length; i++) {
-            int u = prerequisites[i][1];
-            int v = prerequisites[i][0];
+        int[] indegree = new int[n];
+        for (int[] p : prerequisites) {
+            int u = p[1];
+            int v = p[0];
 
             adj.get(u).add(v);
+            indegree[v]++;
         }
-
-        int[] indegree = new int[V];
-        for (int i = 0; i < V; i++) {
-            for (int adjNode : adj.get(i)) {
-                indegree[adjNode]++;
-            }
-        }
-
         Queue<Integer> q = new LinkedList<>();
-        for (int i = 0; i < V; i++) {
-            if (indegree[i] == 0) {
-                q.offer(i);
-            }
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0) q.offer(i);
         }
-
-        ArrayList<Integer> topoSort = new ArrayList<>();
+        List<Integer> topo = new ArrayList<>();
         while (!q.isEmpty()) {
             int node = q.poll();
-            topoSort.add(node);
+            topo.add(node);
 
             for (int adjNode : adj.get(node)) {
                 indegree[adjNode]--;
@@ -46,6 +30,6 @@ class Solution {
             }
         }
 
-        return topoSort.size() == V;
+        return topo.size() == n;
     }
 }
