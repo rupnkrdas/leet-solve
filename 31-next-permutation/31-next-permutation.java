d8ddb854a69af17d715486c19d4a40f9771f21bb
Rupankar Das <88283289+rupnkrdas@users.noreply.github.com>
2023-07-17 12:05:23 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (15.40%) - LeetHub

--- 31-next-permutation/31-next-permutation.java
+++ 31-next-permutation/31-next-permutation.java
@@ -1,39 +1,40 @@
 class Solution {
+    private void swap(int[] nums, int i, int j) {
+        int temp    = nums[i];
+        nums[i]     = nums[j];
+        nums[j]     = temp;
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
+    }
+
     public void nextPermutation(int[] nums) {
-        int n = nums.length;
         int idx = -1;
+        int n = nums.length;
+        
         for (int i = n - 2; i >= 0; i--) {
             if (nums[i] < nums[i + 1]) {
                 idx = i;
                 break;
             }
         }
-
         if (idx == -1) {
             reverse(nums, 0, n - 1);
-        } else {
-            for (int i = n - 1; i > idx; i--) {
-                if (nums[i] > nums[idx]) {
-                    swap(nums, idx, i);
-                    break;
-                }
+            return;
+        }
+
+        for (int i = n - 1; i > idx; i--) {
+            if (nums[i] > nums[idx]) {
+                swap(nums, idx, i);
+                break;
             }
-            reverse(nums, idx + 1, n - 1);
         }
-    }
-
-
-    public void reverse(int[] arr, int i, int j) {
-        while (i < j) {
-            swap(arr, i, j);
-            i++;
-            j--;
-        }
-    }
-
-    public void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+        
+        reverse (nums, idx + 1, n - 1);
     }
 }
