9bb5b290709a7ae7f1cca4011ec339b2449d1fc7
Rupankar Das <88283289+rupnkrdas@users.noreply.github.com>
2023-07-17 14:26:48 +0530

    Time: 9 ms (81.96%), Space: 45.9 MB (23.80%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -1,25 +1,21 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        int n = intervals.length;
-        int[][] arr = intervals;
-        Arrays.sort(intervals, new Comparator<int[]>() {
-            public int compare(int[] row1, int[] row2) {
-                return row1[0] - row2[0];
-            }
-        });
-
-        int[] p = new int[]{arr[0][0], arr[0][1]};
+        Arrays.sort(intervals, (p1, p2) -> Integer.compare(p1[0], p2[0]));
+        int[] pair = {intervals[0][0], intervals[0][1]};
+        
         List<int[]> list = new ArrayList<>();
-        for (int i = 1; i < n; i++) {
-            if (arr[i][0] <= p[1]) {
-                p[1] = Math.max(arr[i][1], p[1]);
-            } else {
-                list.add(new int[]{p[0], p[1]});
-                p[0] = arr[i][0];
-                p[1] = arr[i][1];
+        
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] > pair[1]) { // cannot be merged
+                list.add(new int[]{pair[0], pair[1]});
+                pair[0] = intervals[i][0];
+                pair[1] = intervals[i][1];
+            } else { // merge
+                pair[0] = Math.min(pair[0], intervals[i][0]);
+                pair[1] = Math.max(pair[1], intervals[i][1]);
             }
         }
-        list.add(new int[]{p[0], p[1]});
+        list.add(new int[] {pair[0], pair[1]});
 
         int[][] ans = new int[list.size()][2];
         for (int i = 0; i < list.size(); i++) {
