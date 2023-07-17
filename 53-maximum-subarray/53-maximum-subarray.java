dcb07bd2df5969adbe803a757408e135305e59b1
Rupankar Das <88283289+rupnkrdas@users.noreply.github.com>
2023-07-17 12:08:50 +0530

    Time: 1 ms (100.00%), Space: 60.3 MB (6.63%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -1,14 +1,17 @@
 class Solution {
     public int maxSubArray(int[] nums) {
         int global_max = Integer.MIN_VALUE;
-        int curr_sum = 0;
+        int curr_max = 0;
 
         for (int num : nums) {
-            curr_sum += num;
-            global_max = Math.max(global_max, curr_sum);
-
-            if (curr_sum < 0) {
-                curr_sum = 0;
+            curr_max += num;
+            
+            if (curr_max > global_max) {
+                global_max = curr_max;
+            }
+            if (curr_max < 0) { // if curr_max drops to negative, reset it to 0
+                                // this is the typical Kadane's Algorithm
+                curr_max = 0;
             }
         }
 
