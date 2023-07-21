dc9652b5157dbd6018925eff53e2ad05dccb9a9f
Rupankar Das <88283289+rupnkrdas@users.noreply.github.com>
2023-07-21 16:48:27 +0530

    Time: 2 ms (23.15%), Space: 41 MB (22.63%) - LeetHub

--- 1854-maximum-population-year/1854-maximum-population-year.java
+++ 1854-maximum-population-year/1854-maximum-population-year.java
@@ -1,6 +1,6 @@
 class Solution {
     public int maximumPopulation(int[][] logs) {
-        Map<Integer, Integer> map = new TreeMap<>();
+        Map<Integer, Integer> map = new HashMap<>();
         for (int[] log : logs) {
             map.put(log[0], map.getOrDefault(log[0], 0) + 1);
             map.put(log[1], map.getOrDefault(log[1], 0) - 1);
@@ -8,11 +8,11 @@ class Solution {
 
         int prefix = 0;
         int max_prefix = 0, ans = 0;
-        for (Map.Entry<Integer, Integer> e : map.entrySet()) {
-            prefix += e.getValue();
+        for (int year = 1950; year <= 2050; year++) {
+            prefix += map.getOrDefault(year, 0);
             if (prefix > max_prefix) {
                 max_prefix = prefix;
-                ans = e.getKey();
+                ans = year;
             }
         }
 
