187dcffd8faad07951e75c0c80d86933b3706b26
Rupankar Das <88283289+rupnkrdas@users.noreply.github.com>
2023-07-31 12:37:31 +0530

    Time: 234 ms (21.65%), Space: 44.3 MB (67.68%) - LeetHub

--- 2719-count-of-integers/2719-count-of-integers.java
+++ 2719-count-of-integers/2719-count-of-integers.java
@@ -1,56 +1,51 @@
 class Solution {
-    int MOD = (int)(1e9 + 7);
-    int[][][][] dp;
-    public int count(String num1, String num2, int min_sum, int max_sum) {
-        dp = new int[23][2][2][401];
-        for (int i = 0; i < 23; i++) {
-            for (int j = 0; j < 2; j++) {
-                for (int k = 0; k < 2; k++) {
-                    for (int l = 0; l < 401; l++) {
-                        dp[i][j][k][l] = -1;
-                    }
-                }
-            }
+    Map<String, Long> map = new HashMap<>();
+    private long MOD = (long)(1e9 + 7);
+    private int getSum(String s) {
+        int sum = 0;
+        for (int i = 0; i < s.length(); i++) {
+            sum += s.charAt(i) - '0';
         }
 
-        String num1extended = "";
-        int n1 = num1.length();
-        int n2 = num2.length();
-        int d = n2 - n1;
-        for (int i = 0; i < d; i++) {
-            num1extended += '0';
-        }
-
-        num1extended += num1;
-
-
-        int a = countStrings(0, 1, 1, num1extended.toCharArray(), num2.toCharArray(), max_sum);
-        int b = countStrings(0, 1, 1, num1extended.toCharArray(), num2.toCharArray(), min_sum - 1);
-
-        int ans = (a - b) % MOD;
-
-        return (ans < 0) ? ans + MOD : ans;
+        return sum;
     }
-
-    // here sum denotes "X - digit_sum"
-    public int countStrings(int i, int tight1, int tight2, char[] num1, char[] num2, int sum) {
-        if (sum < 0)
-            return 0; // if sum < 0, not possible
-        if (i == num2.length)
-            return 1; // if reached end, return 1;
-
-        if (dp[i][tight1][tight2][sum] != -1) {
-            return dp[i][tight1][tight2][sum];
+    private long solve(int idx, String num, int sum, boolean isTight, int minSum, int maxSum) {
+        if (idx == num.length()) {
+            if (sum >= minSum && sum <= maxSum) return 1;
+            return 0;
         }
 
-        int lo = (tight1 == 1) ? num1[i] - '0' : 0; // if tight1 is tight, then don't start from 0
-        int hi = (tight2 == 1) ? num2[i] - '0' : 9; // if tight2 is tight, then don't end at 9
+        String key = Integer.toString(idx) + "," + Integer.toString(sum) + "," + Boolean.toString(isTight);
+        if (map.containsKey(key)) return map.get(key);
 
-        int count = 0;
-        for (int idx = lo; idx <= hi; idx++) {
-            count = count % MOD + countStrings(i + 1, ((tight1 == 1) && (idx == lo)) ? 1 : 0, ((tight2 == 1) && (idx == hi)) ? 1 : 0, num1, num2, sum - idx) % MOD;
+        int limit = num.charAt(idx) - '0';
+        if (!isTight) {
+            limit = 9;
+        }
+        
+        long ans = 0;
+        for (int digit = 0; digit <= limit; digit++) {
+            boolean nextTight = isTight && (digit == limit);
+
+            ans = ans%MOD + solve(idx + 1, num, sum + digit, nextTight, minSum, maxSum)%MOD;
         }
 
-        return dp[i][tight1][tight2][sum] = count;
+        map.put(key, ans%MOD);
+        return ans%MOD;
+    }
+    public int count(String num1, String num2, int min_sum, int max_sum) {
+        long ans = solve(0, num2, 0, true, min_sum, max_sum);
+        map.clear();
+        ans -= solve(0, num1, 0, true, min_sum, max_sum);
+        ans = (ans + MOD)%MOD;
+
+        // check for num1
+        int sum = getSum(num1);
+        boolean isNum1Valid = false;
+        if (sum >= min_sum && sum <= max_sum) isNum1Valid = true;
+
+        ans = (ans + (isNum1Valid?1:0) + MOD)%MOD;
+
+        return (int)ans;
     }
 }
