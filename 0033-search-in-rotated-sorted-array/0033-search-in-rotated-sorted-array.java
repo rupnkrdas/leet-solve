cce929281297bc55b54bbdc89465ebd4ea1e6b82
Rupankar Das <88283289+rupnkrdas@users.noreply.github.com>
2023-08-08 19:37:22 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (64.16%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int pivot = findPivot(nums);
+        if (pivot == -1) {
+            return bs(nums, 0, nums.length - 1, target);
+        } else {
+            int ans = bs(nums, 0, pivot, target);
+            return (ans != -1) ? ans : bs(nums, pivot + 1, nums.length - 1, target);
+        }
+    }
+
+    public int findPivot(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        int ans = 0;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (mid + 1 < arr.length && arr[mid] > arr[mid + 1]) {
+                return mid;
+            } else if (arr[mid] > arr[arr.length - 1]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public int bs(int[] arr, int start, int end, int target) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == target) return mid;
+            else if (arr[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
