ab1ffa2aa334db0652d46d50350864ada77e633c
Rupankar Das <88283289+rupnkrdas@users.noreply.github.com>
2023-08-13 15:13:35 +0530

    Time: 17 ms (20.39%), Space: 61.7 MB (33.98%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.java
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.java
@@ -1,12 +1,5 @@
 class Solution {
     int[] dp = new int[(int)(1e5 + 1)];
-    private boolean isIncreasing(List<Integer> list) {
-        for (int i = 1; i < list.size(); i++) {
-            if (list.get(i) - list.get(i - 1) != 1) return false;
-        }
-
-        return true;
-    }
     private boolean f(int idx, int[] nums) {
         if (idx == nums.length) return true;
 
@@ -14,43 +7,21 @@ class Solution {
             return (dp[idx]==1)?true:false;
         }
         
-        Set<Integer> set = new HashSet<>();
         boolean ans = false;
-        boolean flag = true;
-        for (int i = idx; i < Math.min(idx+3,nums.length); i++) {
-            set.add(nums[i]);
-            int len = i - idx + 1;
-            if (i > idx) {
-                if (nums[i]-nums[i-1] != 1) flag = false;
+        int n = nums.length;
+        if (idx + 1 < n && nums[idx] == nums[idx + 1]) {
+            if (f(idx+2, nums)) {
+                ans = true;
             }
-            if (len == 3) {
-                if (set.size() == 1) {
-                    if (f(i + 1, nums)) {
-                        ans = true;
-                        break;
-                    }
-                } else {
-                    if (flag) {
-                        // System.out.println(set.toString());
-                        // List list = new ArrayList<>();
-                        // list.addAll(set);
-                        // if (isIncreasing(list)) {
-                        //     ans = true;
-                        //     break;
-                        // }
-                        if (f(i+1, nums)) {
-                            ans = true;
-                            break;
-                        }
-                    }
-                }
-            } else if (len == 2) {
-                if (set.size() == 1) {
-                    if (f(i+1, nums)) {
-                        ans = true;
-                        break;
-                    }
-                }
+        }
+        if (!ans && idx + 2 < n && nums[idx] == nums[idx + 1] && nums[idx + 1] == nums[idx + 2]) {
+            if (f(idx + 3, nums)) {
+                ans = true;
+            }
+        }
+        if (!ans && idx + 2 < n && nums[idx] + 1 == nums[idx + 1] && nums[idx + 1] + 1 == nums[idx + 2]) {
+            if (f(idx + 3, nums)) {
+                ans = true;
             }
         }
 
