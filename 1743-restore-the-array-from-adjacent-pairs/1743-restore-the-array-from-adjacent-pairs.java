c2affd288da7c1bb29f814ed3eafdd55ee7cfe0e
Rupankar Das <88283289+rupnkrdas@users.noreply.github.com>
2023-11-10 12:49:25 +0530

    Time: 182 ms (9.43%), Space: 110.5 MB (30.74%) - LeetHub

--- 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.java
+++ 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.java
@@ -1,49 +1,48 @@
 class Solution {
-    public void dfs(int node, Map<Integer, Boolean> vis, Map<Integer, List<Integer>> adj, int[] ans, int i) {
-        if (i == ans.length) return;
-
-        ans[i] = node;
-        vis.put(node, true);
+    List<Integer> list = new ArrayList<>();
+    private void dfs(int node, Map<Integer, List<Integer>> adj, Set<Integer> vis) {
+        vis.add(node);
+        list.add(node);
 
         for (int adjNode : adj.get(node)) {
-            if (!vis.containsKey(adjNode)) {
-                dfs(adjNode, vis, adj, ans, i + 1);
-            }
+            if (!vis.contains(adjNode)) dfs(adjNode, adj, vis);
         }
+
+        return;
     }
-    public int[] restoreArray(int[][] ap) {
-        int n = ap.length;
-        n = n + 1;
-        Map<Integer, List<Integer>> adj = new HashMap<>();
+    public int[] restoreArray(int[][] pairs) {
+        // 2 -> 1, 1 -> 2
+        // 3 -> 4, 4 -> 3
+        // 3 -> 2, 2 -> 3
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        int N = pairs.length + 1;
+        
+        Map<Integer, Integer> indegree = new HashMap<>();
+        for (int[] p : pairs) {
+            int u = p[0];
+            int v = p[1];
 
-        for (int[] edge : ap) {
-            int u = edge[0];
-            int v = edge[1];
+            if (!map.containsKey(u)) map.put(u, new ArrayList<>());
+            if (!map.containsKey(v)) map.put(v, new ArrayList<>());
+            map.get(u).add(v);
+            map.get(v).add(u);
+            
+            indegree.put(u, indegree.getOrDefault(u, 0) + 1);
+            indegree.put(v, indegree.getOrDefault(v, 0) + 1);
+        }
 
-            if (adj.containsKey(u)) {
-                adj.get(u).add(v);
-            } else {
-                adj.put(u, new ArrayList<>());
-                adj.get(u).add(v);
-            }
-            if (adj.containsKey(v)) {
-                adj.get(v).add(u);
-            } else {
-                adj.put(v, new ArrayList<>());
-                adj.get(v).add(u);
+        
+        Set<Integer> vis = new HashSet<>();
+        for (int u : map.keySet()) {
+            if (indegree.get(u) == 1) {
+                dfs(u, map, vis);
+                break;
             }
         }
 
-        int src = 0;
-        for (Map.Entry<Integer, List<Integer>> e : adj.entrySet()) {
-            if (e.getValue().size() == 1) {
-                src = e.getKey();
-            }
-        }
+        int[] ans = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) ans[i] = list.get(i);
 
-        int[] ans = new int[n];
-        Map<Integer, Boolean> vis = new HashMap<>();
-        dfs(src, vis, adj, ans, 0);
         return ans;
     }
 }
