a9600b1ac74839aaedc1d2c29dd5c73827db9cdd
Rupankar Das <88283289+rupnkrdas@users.noreply.github.com>
2023-12-22 21:39:50 +0530

    Time: 1 ms (97.78%), Space: 41.4 MB (33.33%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.java
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.java
@@ -2,19 +2,26 @@ class Solution {
     public int maxScore(String s) {
         int n = s.length();
         char[] arr = s.toCharArray();
-
-        int totalZeroes = 0;
-        for (int i = 0; i < n; i++) totalZeroes += (arr[i] == '0') ? 1 : 0;
-        int ones = 0;
-        int zeroes = 0;
-        int maxi = 0;
-        for (int len = 1; len < n; len++) {
-            zeroes += (arr[len - 1] == '0') ? 1 : 0;
-            ones += (arr[len - 1] == '1') ? 1 : 0;
-            
-            maxi = Math.max(maxi, n - ones - (totalZeroes - zeroes));
+        int[] left  = new int[n - 1];
+        int[] right = new int[n - 1];
+        
+        int l = 0;
+        for (int i = 0; i < n - 1; i++) {
+            l += (arr[i] == '0') ? 1 : 0;
+            left[i] = l;
         }
-
+        
+        int r = 0;
+        for (int i = n - 1; i > 0; i--) {
+            r += (arr[i] == '1') ? 1 : 0;
+            right[i - 1] = r;
+        }
+        
+        int maxi = 0;
+        for (int i = 0; i < n - 1; i++) {
+            maxi = Math.max(maxi, left[i] + right[i]);
+        }
+        
         return maxi;
     }
 }
