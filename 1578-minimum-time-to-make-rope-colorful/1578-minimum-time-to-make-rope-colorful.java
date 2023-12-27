94bf8b12c09e056edeca2f657edf8a8a81e1fbae
Rupankar Das <88283289+rupnkrdas@users.noreply.github.com>
2023-12-27 20:18:53 +0530

    Time: 7 ms (88.95%), Space: 61.1 MB (11.83%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.java
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.java
@@ -4,35 +4,27 @@ class Solution {
         char[] arr = colors.toCharArray();
         // a b a a c
         
-        Map<Integer, Integer> map = new HashMap<>();
+        int total = 0;
         for (int i = 0; i < n; i++) {
             char ch = arr[i];
 
             int j = i;
-            while (j + 1 < n && arr[j + 1] == ch) j++;
-            map.put(i, j);
-
-            i = j;
-        }
-
-        // System.out.println(map);
-
-        int total = 0;
-        for (Map.Entry<Integer, Integer> e : map.entrySet()) {
-            int start = e.getKey();
-            int end = e.getValue();
-            if (start == end) continue;
-
-            int maxi = Integer.MIN_VALUE;
-            int sum = 0;
-            for (int i = start; i <= end; i++) {
-                sum += time[i];
-                maxi = Math.max(maxi, time[i]);
+            int maxi = time[i];
+            int sum = time[i];
+            while (j + 1 < n && arr[j + 1] == ch) {
+                j++;
+                sum += time[j];
+                maxi = Math.max(maxi, time[j]);
             }
 
-            // System.out.println(sum - maxi);
+            if (j != i) {
+                total += sum - maxi;
+            }
 
-            total += sum - maxi;
+            // System.out.println(i+","+j);
+            // System.out.println("maxi: " + maxi + " sum: " + sum);
+
+            i = j;
         }
 
         return total;
