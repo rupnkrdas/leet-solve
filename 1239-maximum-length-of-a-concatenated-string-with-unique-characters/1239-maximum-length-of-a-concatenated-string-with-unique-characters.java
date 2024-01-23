8c4caf4a1adc5b3af536b9f4534c860a78f625e3
Rupankar Das <88283289+rupnkrdas@users.noreply.github.com>
2024-01-24 00:40:59 +0530

    Time: 4 ms (93.95%), Space: 41.5 MB (92.34%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
@@ -1,44 +1,33 @@
 class Solution {
-    Map<String, Integer> map = new HashMap<>();
+    int maxi = 0;
+
+    private void f(int idx, List<Integer> brr, int temp) {
+        maxi = Math.max(maxi, Integer.bitCount(temp));
+
+        for (int i = idx; i < brr.size(); i++) {
+            if ((temp & brr.get(i)) == 0) f(i + 1, brr, temp | brr.get(i));
+        }
+    }
     public int maxLength(List<String> arr) {
-        return f(0, "", arr);
-    }
-
-    public int f(int idx, String s, List<String> arr) {
-        if (idx == arr.size()) {
-            return 0;
-        }
-
-        String key = Integer.toString(idx) + "," + s;
-        if (map.containsKey(key)) return map.get(key);
-
-        String temp = s;
-        int take = 0;
-        if (canTake(s, arr.get(idx))) {
-            take = arr.get(idx).length() + f(idx + 1, s + arr.get(idx), arr);
-        }
-        int notTake = f(idx + 1, temp, arr);
-
-        int ans = Math.max(take, notTake);
-        map.put(key, ans);
-        return ans;
-    }
-
-    public boolean canTake(String s2, String s1) {
-        char[] map = new char[26];
-        for (int i = 0; i < s1.length(); i++) {
-            if (map[s1.charAt(i) - 'a'] >= 1) {
-                return false;
+        List<Integer> brr = new ArrayList<>();
+        for (String s: arr) {
+            Set<Character> set = new HashSet<>();
+            for (int i = 0; i < s.length(); i++) {
+                set.add(s.charAt(i));
             }
-            map[s1.charAt(i) - 'a']++;
-        }
 
-        for (int i = 0; i < s2.length(); i++) {
-            if (map[s2.charAt(i) - 'a'] >= 1) {
-                return false;
+            if (set.size() == s.length()) {
+                int mask = 0;
+                for (int i = 0; i < s.length(); i++) {
+                    mask |= 1 << (s.charAt(i) - 'a');
+                }
+
+                brr.add(mask);
             }
         }
 
-        return true;
+        f(0, brr, 0);
+
+        return maxi;
     }
-} 
+}
